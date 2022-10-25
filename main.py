ba4270118e3a34fe4814107e8a5da780641c4a76
chawije224 <106033248+chawije224@users.noreply.github.com>
2022-10-25 20:47:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -95,7 +95,7 @@ async def input(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 [<code>mpd_link</code>] [<code>license_url</code>]
     
-<b>Eg:</b> <i>https://cdn.bitmovin.com/content/assets/art-of-motion_drm/mpds/11331.mpd https://cwip-shaka-proxy.appspot.com/no_auth</i> <code>Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36</code>  
+<b>Eg:</b> <i>https://cdn.bitmovin.com/content/assets/art-of-motion_drm/mpds/11331.mpd https://cwip-shaka-proxy.appspot.com/no_auth</i>
 ''', parse_mode="HTML")
     elif len(str(msg.text).split(maxsplit=1)) == 2:
         # message.reply('<b>Keys are being extracting . . .</b>')
